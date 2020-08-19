4bdd29108ac1179c5cbc09c8206863df2d103bcd
Artem Diashkin <nemrosim1988@gmail.com>
2020-08-19 22:56:52 +0300

    add timer-rx.js file

---
+++ timer-rx.js
@@ -0,0 +1,15 @@
+const moment = require('moment');
+const {interval} = require('rxjs');
+
+let date = 819151255000;
+
+const source = interval(1000).subscribe(val => {
+
+    date += 1000
+    const res = moment(new Date(date));
+    console.log(res.format('HH:mm:ss'))
+
+    if (val === 5) {
+        source.unsubscribe();
+    }
+});
