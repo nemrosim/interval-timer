cf41374577cfce0fc4a62e60ac6c02f7ffc52853
Artem Diashkin <nemrosim1988@gmail.com>
2020-08-21 08:07:34 +0300

    add new examples

--- rx-examples/13-catch-errors.js
+++ rx-examples/13-catch-errors.js
@@ -1,6 +1,6 @@
 const {of} = require('rxjs');
 const {ajax} = require('rxjs/ajax');
-const {mergeMap, filter, catchError} = require('rxjs/operators');
+const {mergeMap, filter, catchError, throwError} = require('rxjs/operators');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 ajax({
@@ -20,14 +20,17 @@ ajax({
         }
     }),
     // it must be placed in the end of a pipe
-    catchError((error) => {
+    // "caught" -> Observable that produced the error
+    catchError((error, caught) => {
         console.log("🆘 Error!", error.message)
 
         /**
          * We can return a new Observable
          */
         return of('🤬🧨😡 Some weird Error')
-    })
+    }),
+    // "throwError" -> returns error as Observable
+    catchError(err => throwError('some message')),
 )
     .subscribe(timezone => console.log("✔️", timezone),
         error => console.log('Error'),
